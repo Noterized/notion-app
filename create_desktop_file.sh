4b7e00c43a9250cb869403e0444dd2b280b9ae93
david <davidj@sysdrum.com>
2018-05-28 05:59:43 -0500

    Building git repo

---
+++ create_desktop_file.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+WORKING_DIR=`pwd`
+THIS_PATH=`readlink -f $0`
+cd `dirname ${THIS_PATH}`
+FULL_PATH=`pwd`
+cd ${WORKING_DIR}
+cat <<EOS > Notion.desktop
+[Desktop Entry]
+Name[en_US]=Notion.so
+Comment=Notion.so Desktop application for Linux
+Exec="${FULL_PATH}/notion-app"
+Terminal=false
+Type=Application
+Icon=${FULL_PATH}/notion.png
+EOS
+chmod +x Mattermost.desktop
